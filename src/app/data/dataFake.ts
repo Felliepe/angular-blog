975487d28c3822f9f7114076801b3b9de1d72b0a
Felliepe <felipe_88ms@hotmail.com>
2023-12-19 16:09:30 -0300

    conteúdo criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'nguyenvana@gmail.com',
+    description: '0987654321',
+    photoCover:
+      'https://blog.aricanduva.com.br/wp-content/uploads/2022/05/tela-de-games-geek-1024x476.jpeg',
+  },
+  {
+    id: '2',
+    title: 'na@gmail.com',
+    description: '0987675756756756756756321',
+    photoCover:
+      'https://www.maplebearvinhedo.com.br/img/blog/grande/conheca-os-beneficios-de-aprender-programacao-na-infancia-1627481204.jpg',
+  },
+];
