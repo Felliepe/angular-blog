20bfb66a5ce497e77d2932b566b302a11e18d031
Felliepe <felipe_88ms@hotmail.com>
2024-01-15 15:51:12 -0300

    Outras configurações e mudanças feitas

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,16 +1,17 @@
 export const dataFake = [
   {
     id: '1',
-    title: 'nguyenvana@gmail.com',
+    title: 'Divertida Mente 2 será lançado em 2024',
     description: '0987654321',
     photoCover:
       'https://blog.aricanduva.com.br/wp-content/uploads/2022/05/tela-de-games-geek-1024x476.jpeg',
   },
   {
     id: '2',
-    title: 'na@gmail.com',
-    description: '0987675756756756756756321',
+    title: 'Divertida Mente 2 será lançado em 2024',
+    description:
+      'A Disney divulgou um vídeo, um cartaz e a data de estreia do longa nos cinemas: junho de 2024, nove anos depois do primeiro.',
     photoCover:
-      'https://www.maplebearvinhedo.com.br/img/blog/grande/conheca-os-beneficios-de-aprender-programacao-na-infancia-1627481204.jpg',
+      'https://s2-gshow.glbimg.com/BhuFl-NTYFg-XHlrDc-ahKZ0Ey4=/0x0:1086x652/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2021/y/L/h2qWYuSKG7mJJicCQuHw/divertida-mente.jpg',
   },
 ];
